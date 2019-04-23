b482eec9b0557f63c3efc28d76bd1d139c342900
abdel <abdelrahimelshorbgy@gmaol.com>
2019-04-24 01:43:26 +0200

    Preparing the environment & Finishing Info Activity

---
+++ app/src/main/java/com/example/emc/InfoActivity.java
@@ -0,0 +1,18 @@
+package com.example.emc;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+    }
+}
--- app/src/main/java/com/example/emc/MainActivity.java
+++ app/src/main/java/com/example/emc/MainActivity.java
@@ -1,22 +1,80 @@
 package com.example.emc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CardView galleryCardView;
+    private CardView eventsCardView;
+    private CardView educationalCardView;
+    private CardView infoCardView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        galleryCardView = findViewById(R.id.cv_gallery);
+        eventsCardView = findViewById(R.id.cv_events);
+        educationalCardView = findViewById(R.id.cv_edu);
+        infoCardView = findViewById(R.id.cv_info);
 
-        myRef.setValue("Hello, World!");
+        galleryCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        eventsCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        educationalCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EduActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        infoCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.staff){
+            Intent intent = new Intent(MainActivity.this, StaffActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.staff, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
